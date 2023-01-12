a17423f87d695d5a3f4710a6dbb1298e1e9030fe
BenjaminBrugghe <Brugghe.b@gmail.com>
2023-01-12 15:52:52 +0100

    Ajout des onChange + useStates pour Formulaire De Connexion

--- projet-fil-rouge-ib/src/components/FormulaireConnexion.jsx
+++ projet-fil-rouge-ib/src/components/FormulaireConnexion.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import '../styles/formulaireConnexion.css'
 
 const FormulaireConnexion = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function seConnecterClick() {
+        // Vérifier si l'email existe
+        // Vérifier si le MdP correspond
+        // Rediriger vers la page en fonction du role (client, prestataire, admin)
+    }
+
     return (
         <div className='formulaireConnexion'>
             <p className='bonjour'>Bonjour !</p>
             <p className='connectezVous'>Connectez-vous pour avoir accès à plus de contenu !</p>
-            <form className='formConnexion' action="submit">
+            <form className='formConnexion'>
                 <label className='labelConnexion' htmlFor="email">Email
                     <span className='labelSpan'>champ requis</span>
                 </label>
-                <input className='inputConnexion' type="text" />
+                <input className='inputConnexion' type="text" onChange={(e) => setEmail(e.target.value)} />
                 <label className='labelConnexion' htmlFor="motDePasse">Mot de passe
                     <span className='labelSpan labelSpanDeux'>champ requis</span>
                 </label>
-                <input className='inputConnexion' type="password" />
-                <button className='boutonSeConnecter'>Se Connecter</button>
+                <input className='inputConnexion' type="password" onChange={(e) => setPassword(e.target.value)} />
+                <button className='boutonSeConnecter' onClick={seConnecterClick}>Se Connecter</button>
             </form>
             <span className='nouveauPrestataire'>Nouveau Prestataire ?</span>
             <span><a className='lienCreerUnCompte' href="#">Créer un compte</a></span>
