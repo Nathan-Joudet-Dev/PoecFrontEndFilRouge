273805ca172d424d2b8c2c52d4d9e53a0f5f90a1
BenjaminBrugghe <Brugghe.b@gmail.com>
2023-01-13 14:59:16 +0100

    Feat: Création des pages d'inscription (inscription fonctionnelle) :sparkles:

--- projet-fil-rouge-ib/src/components/FormulaireInscription.jsx
+++ projet-fil-rouge-ib/src/components/FormulaireInscription.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/formulaireInscription.css'
+import FormulaireInscriptionDeux from './FormulaireInscriptionDeux';
 
 const FormulaireInscription = () => {
+
+    const _navigate = useNavigate();
+    const [nomSociete, setNomSociete] = useState('')
+    const [email, setEmail] = useState('')
+    const [motDePasse, setMotDePasse] = useState('')
+    const [confirmationMotDePasse, setConfirmationMotDePasse] = useState('')
+
+    const suivantClick = () => {
+        // Pour vérifier que les deux mots de passes correspondent
+        if (motDePasse != confirmationMotDePasse) {
+            alert("Erreur, les deux mots de passe doivent correspondre")
+        } else {
+            localStorage.setItem('nomSociete', nomSociete)
+            localStorage.setItem('email', email)
+            localStorage.setItem('motDePasse', motDePasse)
+            _navigate('/inscriptionSuite')
+        }
+    }
+
     return (
         <div className='formulaireInscription'>
             <p className='devenirPresta'>Devenir Prestataire !</p>
             <form className='formInscription'>
-
                 <label className='labelInscription' htmlFor="nom">Nom Société
                     <span className='labelSpan'>*</span>
                 </label>
-                <input className='inputInscription' type="text" onChange={''} />
-
+                <input className='inputInscription' type="text" onChange={(e) => setNomSociete(e.target.value)} />
                 <label className='labelInscription' htmlFor="email">Email
                     <span className='labelSpan'>*</span>
                 </label>
-                <input className='inputInscription' type="email" onChange={''} />
-
+                <input className='inputInscription' type="email" onChange={(e) => setEmail(e.target.value)} />
                 <label className='labelInscription' htmlFor="password">Mot de Passe
                     <span className='labelSpan'>*</span>
                 </label>
-                <input className='inputInscription' type="password" onChange={''} />
-
+                <input className='inputInscription' type="password" onChange={(e) => setMotDePasse(e.target.value)} />
                 <label className='labelInscription' htmlFor="confirmPassword">Confirmation du Mot de Passe
                     <span className='labelSpan'>*</span>
                 </label>
-                <input className='inputInscription' type="password" onChange={''} />
-
+                <input className='inputInscription' type="password" onChange={(e) => setConfirmationMotDePasse(e.target.value)} />
                 <p className='obligatoires'><span className='etoileObligatoire'>*</span> champs obligatoires</p>
-                <button className='boutonSuivant' onClick={''}>Suivant</button>
+                <button className='boutonSuivant' onClick={suivantClick}>Suivant</button>
             </form>
         </div>
     );
--- projet-fil-rouge-ib/src/components/FormulaireInscriptionDeux.jsx
+++ projet-fil-rouge-ib/src/components/FormulaireInscriptionDeux.jsx
@@ -1,45 +1,80 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Service from '../assets/ApiService'
 import '../styles/formulaireInscriptionDeux.css'
 
 const FormulaireInscriptionDeux = () => {
+
+    const _service = new Service()
+
+    const [nomSociete, setNomSociete] = useState('')
+    const [email, setEmail] = useState('')
+    const [motDePasse, setMotDePasse] = useState('')
+
+    const [logo, setLogo] = useState('')
+    const [description, setDescription] = useState('')
+    const [telephone, setTelephone] = useState('')
+    const [adresse, setAdresse] = useState('')
+    const [pays, setPays] = useState('')
+    const [siret, setSiret] = useState('')
+    const [ville, setVille] = useState('')
+    const [effectif, setEffectif] = useState('')
+    const [domaine, setDomaine] = useState('')
+    const [zoneGeo, setZoneGeo] = useState('')
+
+    useEffect(() => {
+        setNomSociete(localStorage.getItem('nomSociete'))
+        setEmail(localStorage.getItem('email'))
+        setMotDePasse(localStorage.getItem('motDePasse'))
+    }, [])
+
+    async function terminerClick() {
+        const nouvelUtilisateur = {
+            nomSociete: nomSociete,
+            email: email,
+            motDePasse: motDePasse,
+            logo: logo,
+            description: description,
+            telephone: telephone,
+            adresse: adresse,
+            pays: pays,
+            siret: siret,
+            ville: ville,
+            effectif: effectif,
+            domaine: domaine,
+            zoneGeo: zoneGeo
+        }
+        _service.creerUtilisateur(nouvelUtilisateur);
+    }
+
     return (
         <div className='formulaireInscriptionDeux'>
             <p className='devenirPresta'>Devenir Prestataire !</p>
-
             <form className='formInscriptionDeux'>
                 <label className='labelInscription' htmlFor="Logo">Logo</label>
-                <input className='inputInscription inputLogo' type="file" />
-
+                <input className='inputInscription inputLogo' type="file" onChange={(e) => setLogo(e.target.value)} />
                 <label className='labelInscription' htmlFor="Description">Description de l'entreprise</label>
-                <textarea className='inputInscription inputDescription' type="text" />
-
+                <textarea className='inputInscription inputDescription' type="text" onChange={(e) => setDescription(e.target.value)} />
                 <div className='labelFlex'>
                     <label className='labelInscription' htmlFor="Téléphone">Téléphone</label>
-                    <input className='inputInscription inputFlex' type="phone" />
-
+                    <input className='inputInscription inputFlex' type="phone" onChange={(e) => setTelephone(e.target.value)} />
                     <label className='labelInscription' htmlFor="Adresse">Adresse</label>
-                    <input className='inputInscription inputFlex' type="text" />
-
+                    <input className='inputInscription inputFlex' type="text" onChange={(e) => setAdresse(e.target.value)} />
                     <label className='labelInscription' htmlFor="Pays">Pays</label>
-                    <input className='inputInscription inputFlex' type="text" />
-
+                    <input className='inputInscription inputFlex' type="text" onChange={(e) => setPays(e.target.value)} />
                     <label className='labelInscription' htmlFor="SIRET">SIRET</label>
-                    <input className='inputInscription inputFlex' type="text" />
-
+                    <input className='inputInscription inputFlex' type="text" onChange={(e) => setSiret(e.target.value)} />
                     <label className='labelInscription' htmlFor="Ville">Ville</label>
-                    <input className='inputInscription inputFlex' type="text" />
-
+                    <input className='inputInscription inputFlex' type="text" onChange={(e) => setVille(e.target.value)} />
                     <label className='labelInscription' htmlFor="Effectif">Effectif</label>
-                    <input className='inputInscription inputFlex' type="text" />
+                    <input className='inputInscription inputFlex' type="text" onChange={(e) => setEffectif(e.target.value)} />
                 </div>
-
                 <label className='labelInscription' htmlFor="DomainePrincipal">Domaine Principal d'activité</label>
-                <input className='inputInscription inputBottom' type="text" />
-
+                <input className='inputInscription inputBottom' type="text" onChange={(e) => setDomaine(e.target.value)} />
                 <label className='labelInscription' htmlFor="ZoneGeographique">Zone géographique couverte</label>
-                <input className='inputInscription inputBottom' type="text" />
+                <input className='inputInscription inputBottom' type="text" onChange={(e) => setZoneGeo(e.target.value)} />
             </form>
-            <button className='boutonTerminer' onClick={''}>Terminer l'inscription</button>
+            <button className='boutonTerminer' onClick={terminerClick}>Terminer l'inscription</button>
         </div>
     );
 };
