71b26319afd58ea5c2ef44ba8ca4d1f38b4e5403
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-01-24 16:18:18 +0100

    Fix: Modifications Service.js pour récuperer panier client et prestataire + noter une prestation

--- projet-fil-rouge-ib/src/assets/ApiService.js
+++ projet-fil-rouge-ib/src/assets/ApiService.js
@@ -73,34 +73,60 @@ export default class Service {
   }
 
   /**
-   * Met à jour le panier de l'utilisateur
-   * @param {number} id L'id de l'utilisateur
-   * @param {Prestations} panier Le panier de l'utilisateur
-   * @returns L'utilisateur avec son panier modifié
+   * Récupère le panier du client
+   * @param {Utilisateurs} client Le client
+   * @returns Le panier du client
    */
-  async modifierPanierUtilisateur(id, panier) {
-    const response = await axios.patch(
-      _url + `/utilisateurs/${id}`,
-      panier
+  async recupererPanierClient(client) {
+    const prestations = await this.recupererPrestations();
+    const panierClient = await prestations.filter(
+      (prestation) => prestation.client === client.nom && prestation.etat !== "Disponible"
     );
-    const utilisateurModifie = response.data;
-    return utilisateurModifie;
+    return panierClient;
   }
 
   /**
-   * Met à jour le panier du prestataire
-   * @param {number} id 
-   * @param {Prestations} panier 
-   * @returns Le prestataire avec son panier modifié
+   * Récupère le panier du prestataire
+   * @param {Prestataires} prestataire 
+   * @returns Le panier du prestataire
    */
-  async modifierPanierPrestataire(id, panier) {
-    const response = await axios.patch(
-      _url + `/prestataires/${id}`,
-      panier
+  async recupererPanierPrestataire(prestataire) {
+    const prestations = await this.recupererPrestations();
+    const panierPrestataire = await prestations.filter(
+      (prestation) => prestation.prestataire === prestataire.nomSociete && prestation.etat !== "Disponible"
     );
-    const prestataireModifie = response.data;
-    return prestataireModifie;
+    return panierPrestataire;
   }
+
+  // /**
+  //  * Met à jour le panier de l'utilisateur
+  //  * @param {number} id L'id de l'utilisateur
+  //  * @param {Prestations} panier Le panier de l'utilisateur
+  //  * @returns L'utilisateur avec son panier modifié
+  //  */
+  // async modifierPanierUtilisateur(id, panier) {
+  //   const response = await axios.patch(
+  //     _url + `/utilisateurs/${id}`,
+  //     panier
+  //   );
+  //   const utilisateurModifie = response.data;
+  //   return utilisateurModifie;
+  // }
+
+  // /**
+  //  * Met à jour le panier du prestataire
+  //  * @param {number} id 
+  //  * @param {Prestations} panier 
+  //  * @returns Le prestataire avec son panier modifié
+  //  */
+  // async modifierPanierPrestataire(id, panier) {
+  //   const response = await axios.patch(
+  //     _url + `/prestataires/${id}`,
+  //     panier
+  //   );
+  //   const prestataireModifie = response.data;
+  //   return prestataireModifie;
+  // }
 
   // ************************** Prestations **************************
 
@@ -167,18 +193,27 @@ export default class Service {
     return prestationModifiee;
   }
 
+  // /**
+  //  * Ajoute une prestation au panier de l'utilisateur
+  //  * @param {number} id L'id de l'utilisateur
+  //  * @param {Prestation} prestation La prestation à ajouter au panier
+  //  * @returns L'utilisateur avec son panier modifié
+  //  */
+  // async ajouterPrestationAuPanier(id, prestation) {
+  //   const utilisateur = await this.recupererUtilisateurById(id);
+  //   utilisateur.panier.push(prestation);
+  //   const response = await axios.put(_url + `/utilisateurs/${id}`, utilisateur);
+  //   const utilisateurModifie = response.data;
+  //   return utilisateurModifie;
+  // }
 
-  /**
-   * Ajoute une prestation au panier de l'utilisateur
-   * @param {number} id L'id de l'utilisateur
-   * @param {Prestation} prestation La prestation à ajouter au panier
-   * @returns L'utilisateur avec son panier modifié
-   */
-  async ajouterPrestationAuPanier(id, prestation) {
-    const utilisateur = await this.recupererUtilisateurById(id);
-    utilisateur.panier.push(prestation);
-    const response = await axios.put(_url + `/utilisateurs/${id}`, utilisateur);
-    const utilisateurModifie = response.data;
-    return utilisateurModifie;
+  async noterPrestation(prestationNotee) {
+
+    const response = await axios.put(
+      _url + `/prestations/${prestationNotee.id}`,
+      prestationNotee
+    );
+    const prestationModifiee = response.data;
+    return prestationModifiee;
   }
 }
