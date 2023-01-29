c261fe53e134b5ce1c689a7c431635a899d39c68
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-01-29 13:18:55 +0100

    Fix: ajout du logo UltraMotion dans le Header prestaEtAdmin

--- projet-fil-rouge-ib/src/components/HeaderPrestaEtAdmin.jsx
+++ projet-fil-rouge-ib/src/components/HeaderPrestaEtAdmin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { useNavigate, Link } from 'react-router-dom';
+import logo from "../data/images/logo.png";
+import '../styles/headerPrestaEtAdmin.css'
 
 const HeaderPrestaEtAdmin = ({ nom, arrow, boutonAjouter }) => {
 
@@ -13,6 +15,7 @@ const HeaderPrestaEtAdmin = ({ nom, arrow, boutonAjouter }) => {
     return (
         <div className='headerConnection'>
             <AiOutlineArrowLeft className={arrow} onClick={retour} />
+            <img className="logo logoHeader" src={logo} alt="Logo UltraMotionCorp" />
             <div className='headercontainer'>
                 <span className='texte'>{nom}</span>
             </div>
